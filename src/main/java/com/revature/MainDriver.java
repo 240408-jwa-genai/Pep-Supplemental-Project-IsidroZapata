ba962630e6e781c044d012942dac0e5dddbba88b
IsidroZ98 <isidro.zapata01@utrgv.edu>
2024-04-12 18:18:34 -0500

    added very surface level methods for user creation

--- src/main/java/com/revature/MainDriver.java
+++ src/main/java/com/revature/MainDriver.java
@@ -10,65 +10,60 @@ import com.revature.utilities.ConnectionUtil;
 
 public class MainDriver {
 
+    /*
+     * An example of how to get started with the registration business and software
+     * requirements has been
+     * provided in this code base. Feel free to use it yourself, or adjust it to
+     * better fit your own vision
+     * of the application. The affected classes are:
+     * MainDriver
+     * UserController
+     * UserService
+     */
     public static UserDao userDao = new UserDao();
     public static UserService userService = new UserService(userDao);
     public static UserController userController = new UserController(userService);
 
-    /*
-        An example of how to get started with the registration business and software requirements has been
-        provided in this code base. Feel free to use it yourself, or adjust it to better fit your own vision
-        of the application. The affected classes are:
-            MainDriver
-            UserController
-            UserService
-     */
     public static void main(String[] args) {
         // TODO: implement main method to initialize layers and run the application
 
-        // We are using a Try with Resources block to auto close our scanner when we are done with it
-        try (Scanner scanner = new Scanner(System.in)){
-            /*
-                The userIsActive boolean is used to control our code loop. While the user is "active" the code
-                will loop. When prompted, the user can enter "q" to quit the program
-             */
-            boolean userIsActive = true;
-            while (userIsActive){
-                System.out.println("\nHello! Welcome to the Planetarium! Enter 1 to register an account, 2 to log in, q to quit");
-                String userChoice = scanner.nextLine();
-                if (userChoice.equals("1")){
-                    // remind the user of the choice they made
-                    System.out.println("You chose to register an account!");
+        // We are using a Try with Resources block to auto close our scanner when we are
+        // done with it
+        try (Scanner scanner = new Scanner(System.in)) {
+            boolean activeSession = true;
+            while (activeSession) {
+                System.out.println(
+                        "\nHello! Welcome to our Planetarium! Enter 1 to register an account, 2 to log in, or q to quit this session.");
+                String sessionChoice = scanner.nextLine();
+                if (sessionChoice.equals("1")) {
+                    System.out.println("You have chosen to register your account to our Planetarium!");
 
-                    // get the prospective username of the new user
-                    System.out.print("Please enter your desired username: ");
+                    System.out.print("Please enter the username for your account: ");
                     String potentialUsername = scanner.nextLine();
 
-                    // get the prospective password of the new yser
-                    System.out.print("Please enter your desired password: ");
+                    System.out.print("Please enter the password for your account: ");
                     String potentialPassword = scanner.nextLine();
+                    // Create a User Object and provide it with the username and password
+                    // keep in mind the id wil be set by the DB
+                    // (Future note sql - auto increment remember to ignore 1st param)
 
-                    // create a User object and provide it with the username and password
-                    // keep in mind the id will be set by the database if the username and password
-                    // are valid
                     User potentialUser = new User();
                     potentialUser.setUsername(potentialUsername);
                     potentialUser.setPassword(potentialPassword);
 
                     // pass the data into the service layer for validation
-                    userController.register(potentialUser);
-
-                } else if (userChoice.equals("2")){
-                    System.out.println("You chose to log in!");
-                } else if (userChoice.equals("q")) {
-                    System.out.println("Goodbye!");
-                    userIsActive = false;
+                    userService.register(potentialUser);
+                } else if (sessionChoice.equals("2")) {
+                    System.out.println("You have chosen to log in!");
+                } else if (sessionChoice.equals("q")) {
+                    System.out.println("Thank you for visiting our Planetarium, Goodbye World!");
+                    activeSession = false;
                 } else {
-                    System.out.println("Invalid choice, please try again");
+                    System.out
+                            .println("This is not a valid option, please choose from one of the options printed below");
                 }
             }
-
         }
-
 
     }
 
--- src/main/java/com/revature/service/UserService.java
+++ src/main/java/com/revature/service/UserService.java
@@ -8,7 +8,7 @@ public class UserService {
 
 	private UserDao dao;
 
-	public UserService(UserDao dao){
+	public UserService(UserDao dao) {
 		this.dao = dao;
 	}
 
@@ -18,60 +18,37 @@ public class UserService {
 	}
 
 	public User register(User registerRequestData) {
-		/*
-			there are two software requirements I need to check in this method:
-				username needs to be unique
-				username/password need to be a maximum of 30 characters
-		 */
-		// we can check the length of the username and password without needing any information from the
-		// database that has other user information. This is a simple if else check
-		if (registerRequestData.getUsername().length() <= 30 && registerRequestData.getPassword().length() <= 30){
-			/*
-				This block of code should only be reached if the username and password are the proper length. If
-				that is the case, we can then check that the username is unique, since there is no point in making
-				this check if the username and/or password is the incorrect length
-			 */
-			// NOTE: the dao method currently returns a null value
-			User databaseData = dao.getUserByUsername(registerRequestData.getUsername());
+		// 2 reqs username to be unique && username and passwords should not be longer
+		// than 30 chars
+		if (registerRequestData.getUsername().length() <= 30 && registerRequestData.getPassword().length() <= 30) {
+			// This block of code should be reached if both meet req
 
-			/*
-				Using the dao method to grab any account that already has the username the registering user
-				is trying to use, we can check if the username is already in use. If not, we can go through with
-				creating the new account, but if it is in use, we send back an empty User object to inform the
-				controller layer that the account could not be made.
-			 */
+			// currently DAO returns null
+			// TODO: Update this next week when squeel works
+			User databaseDAO = dao.getUserByUsername(registerRequestData.getUsername());
 
-			/*
-				I am checking two things here:
-					1. is the usernameFromDatabase object not null (meaning there is a getUsername method I can call)
-					2. assuming a User object was returned, does it have the same username as the register request username
+			// Using the dao method to grab any account
 
-			 */
-
-			// if the data from the database is null then something has gone wrong and I should give a fail
-			// message to the user
-			if(databaseData != null){
-				// for the sake of readability I am saving the data I work with in its own variables
-				String usernameFromDatabase = databaseData.getUsername();
-				String usernameFromRegisterRequest = registerRequestData.getUsername();
-				// checking if the usernames are not the same
-				if (!usernameFromDatabase.equals(usernameFromRegisterRequest)){
-					// if the data is not the same, then the credentials are valid and I can go through
-					// with account registration. Note the registration method requires a new object:
-					// a UsernamePasswordAuthentication object specifically
-					UsernamePasswordAuthentication validUserCreds = new UsernamePasswordAuthentication();
-					validUserCreds.setUsername(usernameFromRegisterRequest);
-					validUserCreds.setPassword(registerRequestData.getPassword());
-
-					// We can shorten our code a bit and return the User data once it is persisted in the
-					// database. We can return a User object with Data that has been initialized to inform
-					// the controller layer the registration process was successful
-					return dao.createUser(validUserCreds);
+			if (databaseDAO != null) {
+				// example of saving the data we are working with in their own vars
+				String daoUserName = databaseDAO.getUsername();
+				String requestUserName = registerRequestData.getUsername();
+				if (!daoUserName.equals(requestUserName)) {
+					// if the data is not the same, then the creds are valid
+					// IF THE USERNAME IS NOT ALREADY EXISTING CREATE A NEW USER
+					UsernamePasswordAuthentication validUserCredentials = new UsernamePasswordAuthentication();
+					validUserCredentials.setUsername(requestUserName);
+					validUserCredentials.setPassword(requestUserName);
+					// NOTE: return a user object to inform CL the registration processed was
+					// successful
+					return dao.createUser(validUserCredentials);
 				}
 			}
 		}
-		// if any Business/Software requirements are not met, we return an empty user which we can use to
+		// if any business/ software reqs are not met, we return an empty user which we
+		// can use to
 		// inform the controller layer that the registration process failed
 		return new User();
+
 	}
 }

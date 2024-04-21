4696a3b5cfa2d52004a28eb68cafceb91b7d5328
IsidroZ98 <isidro.zapata01@utrgv.edu>
2024-04-21 14:44:20 -0500

    progressive updates to MoonService

--- src/main/java/com/revature/service/MoonService.java
+++ src/main/java/com/revature/service/MoonService.java
@@ -3,38 +3,71 @@ package com.revature.service;
 import java.util.List;
 
 import com.revature.models.Moon;
+import com.revature.models.Planet;
 import com.revature.repository.MoonDao;
+import com.revature.repository.PlanetDao;
 
 public class MoonService {
 
 	private MoonDao dao;
+	private PlanetDao planetDao;
+	public MoonService(MoonDao dao, PlanetDao planetDao) {
 
-	public MoonService(MoonDao dao) {
 		this.dao = dao;
+		this.planetDao = planetDao;
 	}
 
 	public List<Moon> getAllMoons(int currentUserId) {
 		// TODO implement
-		return null;
+		return dao.getAllMoons(currentUserId);
 	}
 
 	public Moon getMoonByName(int myPlanetId, String moonName) {
 		// TODO implement
-		return null;
+		Moon moon = dao.getMoonByName(moonName);
+
+		if( moon != null && moon.getMyPlanetId()==myPlanetId )
+		{
+			return moon;
+		}
+		else{
+			return null;
+		}
 	}
 
 	public Moon getMoonById(int myPlanetId, int moonId) {
 		// TODO Aimplement
-		return null;
+		Moon moon = dao.getMoonById(myPlanetId);
+		if(moon != null && moon.getMyPlanetId() == myPlanetId)
+		{
+			return moon;
+		}
+		else {
+			return null;
+		}
 	}
 
-	public Moon createMoon(Moon m) {
+	public Moon createMoon(int myPlanetId, Moon moon) {
 		// TODO implement
-		return null;
+		if(doesPlanetExist(myPlanetId))
+		{
+			moon.setMyPlanetId(myPlanetId);
+			return dao.createMoon(moon);
+		}
+		else{
+			return null;
+		}
+
+
 	}
 
+	public boolean doesPlanetExist(int myPlanetId)
+	{
+		Planet planet = planetDao.getPlanetById(myPlanetId);
+		return planet != null;
+	}
 	public boolean deleteMoonById(int moonId) {
-		return false;
+		return dao.deleteMoonById(moonId);
 	}
 
 	public List<Moon> getMoonsFromPlanet(int myPlanetId) {
